4d71d80ba4fa8324e86ed9f5e4f01340ddd693a0
Yves Greatti <ygrepo@gmail.com>
2025-11-19 09:44:43 -0500

    Added training

--- src/train.py
+++ src/train.py
@@ -10,6 +10,10 @@ from pathlib import Path
 import sys
 from torch_geometric.loader import DataLoader
 
+
+REPO_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(REPO_ROOT))
+
 from src.utils import (
     setup_logging,
     get_logger,
