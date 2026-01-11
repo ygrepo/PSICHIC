3ea70b6d4eff71f24229f75c99c74ce7a7c8a758
Yves Greatti <ygrepo@gmail.com>
2026-01-11 13:12:59 -0500

    Script changes

--- jobs/train_phase2_medium.sh
+++ jobs/train_phase2_medium.sh
@@ -8,8 +8,8 @@
 #BSUB -q gpu
 #BSUB -gpu "num=1"
 #BSUB -R h10080g
-#BSUB -n 16
-#BSUB -R "rusage[mem=80G]"
+#BSUB -n 2
+#BSUB -R "rusage[mem=32G]"
 #BSUB -R "span[hosts=1]"
 #BSUB -W 48:00
 #BSUB -o logs/train_p2.%J.out
